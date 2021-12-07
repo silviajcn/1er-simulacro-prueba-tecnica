7d7259743de2cb93e36a50e7f99724d3a717f1fc
Silvia <silviajcn@gmail.com>
2021-12-07 13:54:06 -0500

    Estilos Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../styles/Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Esta es una pagina de libros</h1>
+        <div className="ContainerPrincipal">
+            <h1 className="tituloPrincipal">La biblioteca de Silvia</h1>
+            <p className="parrafoInicio">Leer es algo que disfruto mucho, por ello, es importante para mi llevar un registro de los autores que he leido y sus respectivos libros.</p>
+
+            <div className="containerImagenes">
+                <img src="https://res.cloudinary.com/silviajcn/image/upload/v1638903169/Im%C3%A1genes%20para%20proyectos/Libros/la-biblioteca_cyrpcv.jpg" alt="..." />
+            </div>
         </div>
     )
 }
